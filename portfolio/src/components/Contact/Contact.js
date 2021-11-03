7d9b9786f7e678e8f0d98777860e1f1b18a6eedb
François-Nicolas Marc-Martin <francoisnicolas.marcmartin@gmail.com>
2021-11-03 11:25:53 +0100

    contact

--- portfolio/src/components/Contact/Contact.js
+++ portfolio/src/components/Contact/Contact.js
@@ -4,8 +4,9 @@ const Contact = () => {
    return (
       <div id="contact" className="contact">
          <div className="contact__information">
+            <p>Email: francoisnicolas.marcmartin@gmail.com</p>
+            <p>Linkedin: <a href="www.linkedin.com/in/francois-nicolas-marc-martin">www.linkedin.com/in/francois-nicolas-marc-martin</a></p>
          </div>
-         <button>Contact</button>
       </div>
    );
 };
