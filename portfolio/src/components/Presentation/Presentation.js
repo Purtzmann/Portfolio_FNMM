c4980be31d282f40951798f3e48fa6fb4f71fa8d
François-Nicolas Marc-Martin <francoisnicolas.marcmartin@gmail.com>
2021-12-22 22:20:18 +0100

    modification section

--- portfolio/src/components/Presentation/Presentation.js
+++ portfolio/src/components/Presentation/Presentation.js
@@ -18,7 +18,7 @@ const Presentation = () => {
                      <p>Bonjour ! Je m'appelle François-Nicolas et j’ai 28 ans. Après quelques années d’expérience dans le marketing, avec une spécialisation pour le CRM et l’analyse de données, j’ai décidé de réorienter mon avenir professionnel.</p>
                      <p>En 2020, j’ai ainsi débuté une reconversion professionnelle pour devenir développeur web, et obtenu un diplôme de développeur front-end via une formation avec Openclassrooms.</p>
                      <p>Aujourd’hui, mon objectif est de trouver un projet ou un travail dans le développement de sites et/ou applications web pour une agence ou une entreprise.</p>
-                     <p>Je code majoritairement en JavaScript et son frameworks REACT.</p>
+                     <p>Je code majoritairement en JavaScript et son frameworks REACT et Vue.js.</p>
                   </div>
                </div>
 
